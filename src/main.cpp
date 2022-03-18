78b5b6f37f43c3e2b405a6a28580788fab4bd616
LuisEliasCampos <101230023+LuisEliasCampos@users.noreply.github.com>
2022-03-17 18:20:25 -0600

    implementacion de los sets y gets de las propiedades privadas de las clases hijas en el main

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,13 @@ int main(){
     Derived2 deri2("2");
 
     deri1.doSomething();
-    cout<<deri2.toString()<<endl;
+    cout<<deri1.toString()<<endl;
+    deri1.setB("primer derivada");
+    cout<<deri1.getB()<<endl;
     cout<<"--------------"<<endl;
     deri2.doSomething();
     cout<<deri2.toString()<<endl;
-
+    deri2.setC("segunda derivada");
+    cout<<deri2.getC()<<endl;
     return 0;
 }
